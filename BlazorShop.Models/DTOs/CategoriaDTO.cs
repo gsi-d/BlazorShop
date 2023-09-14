a45b66d89990e995ed40e9215379496dca96a0db
vlnote62 <vlnote62>
2023-09-14 15:22:42 -0300

    Criando DTOs, Repositório e Controller de Produtos

---
+++ BlazorShop.Models/DTOs/CategoriaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShop.Models.DTOs
+{
+    public class CategoriaDTO
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? IconCSS { get; set; }
+    }
+}
