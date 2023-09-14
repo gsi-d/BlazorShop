7876cde5cecb215345062e747238e1731797a691
vlnote62 <vlnote62>
2023-09-14 10:08:27 -0300

    Criando entidades, configurando o EF Core e aplicando o Migrations

---
+++ BlazorShop.Api/Entities/CarrinhoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorShop.Api.Entities
+{
+    public class CarrinhoItem : EntityBase
+    {
+        public int CarrinhoId { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+
+        public Carrinho? Carrinho { get; set; }
+        public Produto? Produto { get; set; }
+    }
+}
